713f73f59f068877be7e9552a24b88fc3749f668
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-18 21:16:43 -0400

    updated navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -20,19 +20,24 @@ import Link from "next/link";
 
 const pages = [
   {
-    name: "Work",
-    to: "/work",
-    id: "1",
-  },
-  {
     name: "About",
     to: "/about",
-    id: "2",
+    _id: 1,
   },
   {
-    name: "Contact",
+    name: "Work",
+    to: "/work",
+    _id: 2,
+  },
+  {
+    name: "Projects",
+    to: "/projects",
+    _id: 3,
+  },
+  {
+    name: "Contact Me",
     to: "/contact",
-    id: "3",
+    _id: 4,
   },
 ];
 
@@ -51,7 +56,7 @@ export default function Example() {
       >
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+            <span className="sr-only">Haris Iftikhar</span>
             <img
               className="h-8 w-auto"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
@@ -73,7 +78,7 @@ export default function Example() {
         <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-x-6">
           {pages.map((page) => (
             <Link
-              key={page.id}
+              key={page._id}
               href={page.to}
               className="text-sm font-semibold leading-6 text-gray-900"
             >
@@ -92,7 +97,7 @@ export default function Example() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Haris Iftikhar</span>
               <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
@@ -111,60 +116,16 @@ export default function Example() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <Disclosure as="div" className="-mx-3">
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        Product
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? "rotate-180" : "",
-                            "h-5 w-5 flex-none"
-                          )}
-                          aria-hidden="true"
-                        />
-                      </Disclosure.Button>
-                      {/* <Disclosure.Panel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel> */}
-                    </>
-                  )}
-                </Disclosure>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Features
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Marketplace
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Company
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
+                {pages.map((page) => (
+                  <a
+                    href={page.to}
+                    key={page._id}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {page.name}
+                  </a>
+                ))}
+                ;
               </div>
             </div>
           </div>
