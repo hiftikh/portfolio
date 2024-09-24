644be617d395684071b3ba4084f4b96cc1ebc41f
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-24 02:13:51 -0400

    added work json file

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
 import Header from "@/components/Header";
+import ContentBox from "@/components/ContentBox";
 
 export const metadata: Metadata = {
   title: "Work",
 };
 
+import data from "../../json/work.json";
+
 export default function Page() {
   return (
     <>
       <Header title="Work"></Header>
-      <p>This is where my "Work" would go i guess..</p>
+      <p>This is where my Work would go i guess..</p>
+      <ContentBox data={data} />
     </>
   );
 }
