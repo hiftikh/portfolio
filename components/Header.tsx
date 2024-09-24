60593c6c4d88889b0080402af7b537d9442793a8
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-24 02:14:20 -0400

    Removed text-white

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header({ title }: { title: string }) {
   return (
     <>
       <header>
-        <h1 className="text-3xl font-bold text-white py-6">{title}</h1>
+        <h1 className="text-3xl font-bold py-6">{title}</h1>
       </header>
     </>
   );
