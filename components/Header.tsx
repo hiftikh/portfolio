fdbfff77fea02338ac379002cf3202a809d7d973
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-26 03:14:35 -0400

    updated header layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,18 @@
-export default function Header({ title }: { title: string }) {
+import { Lora } from "next/font/google";
+const font = Lora({ subsets: ["latin"], display: "swap" });
+
+export default function Header(params: any) {
+  let className =
+    "text-4xl font-bold font py-4 text-secondary text-center " +
+    `${params.className}`;
+
   return (
     <>
-      <header>
-        <h1 className="text-3xl font-bold py-6">{title}</h1>
+      <header className={font.className}>
+        <h1 className={className}>{params.title}</h1>
+        {params.underline && (
+          <hr className="mx-auto w-1/12 underline h-1 bg-accent border-accent" />
+        )}
       </header>
     </>
   );
