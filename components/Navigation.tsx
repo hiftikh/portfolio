38dbe3ea02f18f15cedff2a874167c38f0026162
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-27 20:03:53 -0400

    updated to sheet(shadcn)

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,26 +1,28 @@
 "use client";
 
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import navigation from "../json/navagation.json";
 import Image from "next/image";
 
-import { usePathname } from "next/navigation";
+import { joinclassNames } from "@/lib/utils";
 
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
-}
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
   const pathname = usePathname();
   return (
     <>
-      <Disclosure as="nav">
+      <nav>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-24 items-center justify-between">
             <div className="flex items-center">
@@ -38,70 +40,71 @@ export default function Navigation() {
             <div className="hidden md:block">
               <div className="ml-4 flex items-center md:ml-6">
                 <div className="ml-10 flex items-baseline space-x-4">
-                  {navigation.map((item) =>
-                    item.show === true ? (
-                      <Link
-                        href={item.href}
-                        key={item.id}
-                        aria-current={
-                          item.href === pathname ? "page" : undefined
-                        }
-                        className={classNames(
-                          "px-2 py-2 text-sm font-normal text-white underline-offset-8 decoration-2",
-                          item.href === pathname
-                            ? "underline font-extrabold decoration-accent text-accent"
-                            : "hover:underline"
-                        )}
-                      >
-                        {item.name}
-                      </Link>
-                    ) : (
-                      ""
-                    )
+                  {navigation.map(
+                    (item) =>
+                      !item.show || (
+                        <Link
+                          href={item.href}
+                          key={item.id}
+                          aria-current={
+                            item.href === pathname ? "page" : undefined
+                          }
+                          className={joinclassNames(
+                            "px-2 py-2 text-sm font-normal text-white underline-offset-8 decoration-2",
+                            item.href === pathname
+                              ? "underline font-extrabold decoration-accent text-accent"
+                              : "hover:underline"
+                          )}
+                        >
+                          {item.name}
+                        </Link>
+                      )
                   )}
                 </div>
               </div>
             </div>
-            <div className="-mr-2 flex md:hidden">
-              {/* Mobile menu button */}
-              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span className="absolute -inset-0.5" />
-                <span className="sr-only">Open main menu</span>
-                <Bars3Icon
-                  aria-hidden="true"
-                  className="block h-6 w-6 group-data-[open]:hidden"
-                />
-                <XMarkIcon
-                  aria-hidden="true"
-                  className="hidden h-6 w-6 group-data-[open]:block"
-                />
-              </DisclosureButton>
-            </div>
+            <Sheet>
+              <SheetTrigger className="md:hidden">
+                <div className="group relative inline-flex items-center justify-center rounded-md p-2 text-secondary hover:bg-accent hover:text-white">
+                  <Bars3Icon
+                    aria-hidden="true"
+                    className="block h-6 w-6 group-data-[open]:hidden"
+                  />
+                </div>
+              </SheetTrigger>
+
+              <SheetContent className="w-screen pt-10">
+                <SheetTitle className="text-center text-accent text-sm hidden">
+                  MENU
+                </SheetTitle>
+                <SheetDescription>
+                  {navigation.map(
+                    (item) =>
+                      !item.show || (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          aria-current={
+                            item.href === pathname ? "page" : undefined
+                          }
+                          className={joinclassNames(
+                            "text-left py-3 block text-3xl text-slate-300",
+                            item.href === pathname
+                              ? "font-bold text-white underline underline-offset-8"
+                              : "hover:text-white"
+                            // "block rounded-md py-3 text-3xl font-light text-center text-secondary"
+                          )}
+                        >
+                          {item.name}
+                        </a>
+                      )
+                  )}
+                </SheetDescription>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
-
-        <DisclosurePanel className="md:hidden">
-          <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            {navigation.map((item) => (
-              <DisclosureButton
-                key={item.name}
-                as="a"
-                href={item.href}
-                aria-current={item.current ? "page" : undefined}
-                className={classNames(
-                  item.current
-                    ? "bg-gray-900 text-white"
-                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                  "block rounded-md px-3 py-2 text-base font-medium"
-                )}
-              >
-                {item.name}
-              </DisclosureButton>
-            ))}
-          </div>
-          <div className="border-t border-gray-700 pb-3 pt-4"></div>
-        </DisclosurePanel>
-      </Disclosure>
+      </nav>
     </>
   );
 }
