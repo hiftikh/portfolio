9671da370cc6931c344512e406463f6d524da1d4
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-27 20:03:18 -0400

    added metatag

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,9 @@
 import Header from "@/components/Header";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Project",
+};
 
 export default function Page() {
   return (
