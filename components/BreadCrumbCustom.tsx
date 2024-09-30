ea7d93a4d1415d35977b7650118cf2a1046e116d
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-29 23:30:10 -0400

    added

---
+++ components/BreadCrumbCustom.tsx
@@ -0,0 +1,39 @@
+import {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+
+export default function BreadCrumbCustom(props: any) {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <Link href="/" className="text-secondary hover:text-slate-500">
+            Home
+          </Link>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <Link
+            href={`${props.currentSubPageURL}`}
+            className="text-secondary hover:text-slate-500"
+          >
+            {props.currentSubPageName}
+          </Link>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage className="text-slate-500">
+            {props.currentPage}
+          </BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
