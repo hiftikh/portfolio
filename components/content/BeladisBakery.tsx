fec376e2f096ff9c9f9b50e6238136485d2e449b
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-29 23:31:00 -0400

    added

---
+++ components/content/BeladisBakery.tsx
@@ -0,0 +1,12 @@
+export default function BeladisBakery() {
+  const classNameBold = "font-bold";
+  return (
+    <>
+      <p className="mb-3">
+        Created dynamic TV Menus using Adobe Photoshop for the graphics and
+        Adobe Premier to create a video of the animation to different menu
+        items. Designed business card.
+      </p>
+    </>
+  );
+}
---
+++ components/content/ForestCityRamadan.tsx
@@ -0,0 +1,11 @@
+export default function ForestCityRamadan() {
+  const classNameBold = "font-bold";
+  return (
+    <>
+      <p className="mb-3">
+        Created 2 published magazine issues using Adobe InDesign and Adobe
+        Photoshop. Printed over 1000 issues that went to local stores.
+      </p>
+    </>
+  );
+}
