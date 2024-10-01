d94fcbd027b69d5a3e892af2d56a010efa309eae
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-01 01:21:22 -0400

    Added icon component using Lucide Icons

---
+++ components/Icon.tsx
@@ -0,0 +1,20 @@
+import dynamic from "next/dynamic";
+import { LucideProps } from "lucide-react";
+import dynamicIconImports from "lucide-react/dynamicIconImports";
+
+interface IconProps extends LucideProps {
+  name: keyof typeof dynamicIconImports;
+}
+
+const Icon = ({ name, ...props }: IconProps) => {
+  const LucideIcon = dynamic(dynamicIconImports[name]);
+  props.size = props.size ? props.size : 14;
+
+  return (
+    <>
+      <LucideIcon {...props} className=" inline pl-1  box-content" />
+    </>
+  );
+};
+
+export default Icon;
