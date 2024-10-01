2d798762c4622be0909b60facc544599e431a3f3
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-01 01:20:54 -0400

    updated

--- components/content/Nurcare.tsx
+++ components/content/Nurcare.tsx
@@ -1,13 +1,45 @@
+import Icon from "@/components/Icon";
 export default function Nurcare() {
-  const classNameBold = "font-bold";
   return (
     <>
+      <h2 className="text-2xl font-bold">Process</h2>
       <p>
-        Designed a logo and business card for a virtual therapy service using
-        Adobe Illustrator. Using the words, <b>Nurture</b> and <b>Care</b>, the
-        idea of <b>Nurcare</b> was born.
+        Designed the logo and business card using{" "}
+        <a
+          href="https://www.adobe.com/ca/products/illustrator.html"
+          target="_blank"
+        >
+          Adobe Illustrator
+          <Icon name="external-link" />
+        </a>
+        and{" "}
+        <a
+          href="https://www.adobe.com/ca/products/indesign.html"
+          target="_blank"
+        >
+          Adobe InDesign
+          <Icon name="external-link" />
+        </a>
+        respectfully.
       </p>
-      <p>he</p>
+      <p>
+        Taking inspriation from the words <b>Noor(Arabic for light)</b>,{" "}
+        <b>Nurture</b>, and <b>Care</b>, the idea of{" "}
+        <b>
+          <i>Nurcare</i>
+        </b>{" "}
+        was born. The green pastel color evoking a calm nurturing atmosphere and
+        the yellow color producing a sense of warmth. The typography is in
+        lowercase and rounded which conveys comfort and softness. The yellow
+        rotated squares on the "u" takes inspiration from the{" "}
+        <a href="https://en.wikipedia.org/wiki/Arabic_alphabet" target="_blank">
+          Arabic Alphabet
+          <Icon name="external-link" />
+        </a>
+        .
+      </p>
+      <h3 className="text-xl font-bold">Icon</h3>
+      <p> </p>
     </>
   );
 }
