b1a1d35912284a31817d5379541b066071f38eb3
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-01 19:22:52 -0400

    add Card interface to params

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -7,6 +7,8 @@ import { notFound } from "next/navigation";
 import data from "@/json/work.json";
 import BreadCrumbCustom from "@/components/BreadCrumbCustom";
 import dynamic from "next/dynamic";
+import Icon from "@/components/Icon";
+import Card from "@/app/interfaces/Card";
 
 export async function generateStaticParams() {
   return data.map((item) => ({
@@ -43,70 +45,46 @@ export default function Page({ params }: any) {
       <div className="w-10/12 mx-auto">
         <BreadCrumbCustom {...breadCrumbObj} />
         <Header underline>{work.name}</Header>
-        {work.description && <About description={work.description} />}
+        {work.description && <About {...work} />}
         <DetailedContent></DetailedContent>
-        {work.img.artwork && <ArtWork artwork={work.img.artwork} />}
-        {work.url && <URLBtn url={work.url} />}
-        {work.keywords && <KeyWords keywords={work.keywords} />}
+        {work.url && <Btn {...work} />}
+        {work.keywords && <KeyWords {...work} />}
       </div>
     </>
   );
 }
 
-const About = ({ description }: any) => {
+const About = ({ description }: Card) => {
   return (
     <>
-      <h2 className="text-2xl font-bold">About</h2>
-      <p className="text-secondary">{description}</p>
+      <h2>About</h2>
+      <p>{description}</p>
     </>
   );
 };
 
-const ArtWork = ({ artwork }: any) => {
-  return (
-    <>
-      <div className="mx-auto my-10 text-center bg w-full md:w-10/12">
-        {artwork.map((art: any, index: number) => (
-          <div key={index} className="mb-10">
-            <Image
-              src={art.url}
-              alt={art.alt}
-              width={1000}
-              height={1000}
-              className="mx-auto"
-            />
-            <p className="py-3 px-3 italic bg-slate-800 text-xs md:text-base">
-              {art.caption}
-            </p>
-          </div>
-        ))}
-      </div>
-    </>
-  );
-};
-
-const URLBtn = ({ url }: any) => {
+const Btn = ({ url }: Card) => {
   return (
     <div className="container mx-auto my-6 w-8/12 text-center">
       <Button
         asChild
         className="my-5 text-primary bg-secondary hover:bg-slate-300 hover:text-primary"
       >
-        <Link href={url} target="_blank">
-          Website
+        <Link href={`${url?.site}`} target="_blank">
+          Website <Icon name="external-link" />
         </Link>
       </Button>
     </div>
   );
 };
 
-const KeyWords = ({ keywords }: any) => {
+const KeyWords = ({ keywords }: Card) => {
   return (
-    <div className="container mx-auto my-6  md:w-8/12 text-center">
-      {keywords.map((keyword: any, index: number) => (
+    <div className="container mx-auto my-6 md:w-8/12 text-center">
+      {keywords?.map((keyword: any, index: number) => (
         <Badge
           variant="outline"
-          className="text-slate-300 border-slate-300 mr-2 px-5 py-2"
+          className="text-slate-300 border-slate-300 mx-1 px-5 py-2"
           key={index}
         >
           {keyword}
