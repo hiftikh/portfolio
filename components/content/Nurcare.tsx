80f1aa61e014b2d42b1b330d9d2ce48207ee5c30
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-30 23:35:38 -0400

    removed mb-3

--- components/content/Nurcare.tsx
+++ components/content/Nurcare.tsx
@@ -2,7 +2,7 @@ export default function Nurcare() {
   const classNameBold = "font-bold";
   return (
     <>
-      <p className="mb-3">
+      <p>
         Designed a logo and business card for a virtual therapy service using
         Adobe Illustrator. Using the words, <b>Nurture</b> and <b>Care</b>, the
         idea of <b>Nurcare</b> was born.
