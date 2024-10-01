cced432fef960f90165d22f19555ae496b0071cb
Haris Iftikhar <hiftikh@uwo.ca>
2024-09-30 23:34:33 -0400

    removed viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,12 +36,11 @@ export const metadata: Metadata = {
   },
 };
 
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-};
+// export const viewport: Viewport = {
+//   width: "device-width",
+//   initialScale: 1,
+//   maximumScale: 1,
+// };
 
 export default function RootLayout({
   children,
