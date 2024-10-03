c809b054202a3f9179baaea677f2bbfaf5121444
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-03 18:58:02 -0400

    Added Project interface

---
+++ app/interfaces/Project.ts
@@ -0,0 +1,24 @@
+export default interface Project {
+  id: number;
+  show: boolean;
+  name: string;
+  slug: string;
+  description?: string;
+  componentName?: string;
+  url?: {
+    site?: string;
+    github?: string;
+  };
+  img: {
+    logo?: {
+      url?: string;
+      alt?: string;
+    };
+    banner?: {
+      url?: string;
+      alt?: string;
+    };
+  };
+  keywords?: string[];
+  dateAdded?: string;
+}
