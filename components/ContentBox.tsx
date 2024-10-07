2751a5879bf69e066e927531b8920ca8c03a2acc
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-07 03:25:41 -0400

    changed hover animation

--- components/ContentBox.tsx
+++ components/ContentBox.tsx
@@ -13,26 +13,28 @@ interface CardProps {
   data: WorkInterance[] | ProjectInterance[];
 }
 
-export const ContentBox: React.FC<CardProps> = ({ data }: CardProps) => {
+export const ContentBox = ({ data }: CardProps) => {
   return (
     <>
-      <div className="my-10 mx-5 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 ">
+      <div className="my-10 mx-5 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
         {data.sort(orderByNewDate).map((item) => (
           <Link
             href={`/work/${item.slug}`}
             key={item.id}
-            className="border-0 rounded-xl bg-transparent overflow-hidden hover:shadow-lg hover:shadow-black/40 transition ease-in-out duration-300s"
+            className="border-0 rounded-xl bg-transparent overflow-hidden group "
           >
             {item.img.banner && (
-              <div className="relative">
+              <div className="relative ">
                 <Image
                   width={600}
                   height={600}
                   src={item.img.banner.url || ""}
                   alt={item.img.banner.alt || ""}
+                  className="group-hover:opacity-80"
                 />
+
                 {item.dateAdded && (
-                  <p className="text-sm absolute font-bold bottom-3 right-3 bg-black/70 outline-black py-1 px-5 rounded-xl">
+                  <p className=" text-sm absolute font-semibold bottom-3 right-3 bg-black/70 outline-black py-1 px-5 rounded-xl">
                     {displayYear(item.dateAdded)}
                   </p>
                 )}
