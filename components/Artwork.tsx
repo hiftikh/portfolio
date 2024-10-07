6730639bca6be93d6b608c91f80ef238dac2cebe
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-07 03:57:00 -0400

    updated layout

--- components/Artwork.tsx
+++ components/Artwork.tsx
@@ -3,19 +3,17 @@ import Image from "next/image";
 export default function Artwork(params: any) {
   return (
     <>
-      <div className="mx-auto my-10 text-center bg w-full md:w-full">
-        <div className="mb-10">
-          <Image
-            src={params.imageURL}
-            alt={params.alt}
-            width={1000}
-            height={1000}
-            className="mx-auto"
-          />
-          <p className="py-3 px-3 font-light bg-slate-800/30 text-xs md:text-base">
-            {params.caption}
-          </p>
-        </div>
+      <div className="my-10 mx-auto w-full md:w-9/12  text-center">
+        <Image
+          src={params.imageURL}
+          alt={params.alt}
+          width={800}
+          height={800}
+          className="mx-auto"
+        />
+        <p className="py-2 px-2 font-light bg-slate-800/30 text-xs md:text-base">
+          {params.caption}
+        </p>
       </div>
     </>
   );
