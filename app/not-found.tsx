2f6a96377ae51bfe38e0d38d309d780ddc588ca2
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-08 17:39:50 -0400

    Added ChangeBgColor component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,33 +1,28 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import ChangeBgColor from "@/components/ChangeBgColor";
 
 export default function NotFound() {
   return (
-    <div className="flex justify-center items-center align-middle text-center w-full h-[70svh]">
-      <div className="p-4">
-        <h1 className="font-extrabold tracking-tight text-6xl mb-4 text-secondary">
-          Whoops!
-        </h1>
-        <h2 className="font-extrabold tracking-tight text-2xl mb-4">
-          This page could not be found sadly..
-        </h2>
-        <Button
-          asChild
-          variant="outline"
-          className="bg-transparent text-secondary mt-6 hover:border-accent"
-        >
-          <Link href="/">Return Home Safely</Link>
-        </Button>
+    <>
+      <ChangeBgColor color="black" />
+      <div className="flex justify-center items-center align-middle text-center w-full h-[70svh]">
+        <div className="p-4">
+          <h1 className="font-extrabold tracking-tight text-6xl mb-4 text-secondary">
+            Whoops!
+          </h1>
+          <h2 className="font-extrabold tracking-tight text-2xl mb-4">
+            This page could not be found sadly..
+          </h2>
+          <Button
+            asChild
+            variant="outline"
+            className="bg-transparent text-secondary mt-6 hover:border-accent"
+          >
+            <Link href="/">Return Home Safely</Link>
+          </Button>
+        </div>
       </div>
-      <style jsx global>
-        {`
-          body {
-            background: black};
-          }
-        `}
-      </style>
-    </div>
+    </>
   );
 }
---
+++ components/ChangeBgColor.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function ChangeBgColor({ color }: any) {
+  return (
+    <>
+      <style jsx global>
+        {`
+    body {
+      background: ${color}};
+    }
+  `}
+      </style>
+    </>
+  );
+}
