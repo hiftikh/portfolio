abfd73b13982638711004760cbdad9b817ac1c75
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-10 01:50:03 -0400

    Updated ClassName and currentSubPage

--- components/BreadCrumbCustom.tsx
+++ components/BreadCrumbCustom.tsx
@@ -1,35 +1,32 @@
 import {
   Breadcrumb,
-  BreadcrumbEllipsis,
   BreadcrumbItem,
-  BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import Link from "next/link";
 
+const ClassName = "text-secondary hover:underline underline-offset-8";
+
 export default function BreadCrumbCustom(props: any) {
   return (
     <Breadcrumb className="mb-4">
       <BreadcrumbList>
         <BreadcrumbItem>
-          <Link href="/" className="text-secondary hover:text-slate-500">
+          <Link href="/" className={ClassName}>
             Home
           </Link>
         </BreadcrumbItem>
         <BreadcrumbSeparator />
         <BreadcrumbItem>
-          <Link
-            href={`${props.currentSubPageURL}`}
-            className="text-secondary hover:text-slate-500"
-          >
+          <Link href={props.currentSubPageURL} className={ClassName}>
             {props.currentSubPageName}
           </Link>
         </BreadcrumbItem>
         <BreadcrumbSeparator />
         <BreadcrumbItem>
-          <BreadcrumbPage className="text-slate-500">
+          <BreadcrumbPage className="text-slate-400">
             {props.currentPage}
           </BreadcrumbPage>
         </BreadcrumbItem>
