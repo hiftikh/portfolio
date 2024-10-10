c3600fb261a5fa5f2c25ed26030488daf913b213
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-10 01:52:55 -0400

    clean up

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,10 +2,8 @@ import { LoaderCircle } from "lucide-react";
 
 export default function Loading() {
   return (
-    <>
-      <div className="text-center mx-auto">
-        <LoaderCircle className="animate-spin text-white text-center h-7 w-7 mx-auto" />
-      </div>
-    </>
+    <div className="text-center mx-auto">
+      <LoaderCircle className="animate-spin text-white text-center h-7 w-7 mx-auto" />
+    </div>
   );
 }
