7a44cd9a691af5ae8185304d412cbd932cd9e912
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-11 03:26:02 -0400

    Added interface, fallback values, and created Caption comp

--- components/Artwork.tsx
+++ components/Artwork.tsx
@@ -1,20 +1,35 @@
 import Image from "next/image";
 
-export default function Artwork(params: any) {
+interface Artwork {
+  caption?: string;
+  imageURL: string;
+  alt: string;
+  width?: number;
+  height?: number;
+}
+
+export default function Artwork(params: Artwork) {
   return (
     <>
-      <div className="my-10 mx-auto w-full md:w-9/12 text-center">
+      <div className="my-10 mx-auto md:w-8/12 text-center">
         <Image
           src={params.imageURL}
-          alt={params.alt}
-          width={800}
-          height={800}
+          alt={params.alt || "Alt Tag"}
+          width={params.width || 800}
+          height={params.height || 800}
           className="mx-auto"
+          style={{ height: "100%", width: "auto" }}
         />
-        <p className="py-2 px-2 font-light bg-slate-800/30 text-xs md:text-s">
-          {params.caption}
-        </p>
+        {params.caption && <Caption {...params} />}
       </div>
     </>
   );
 }
+
+const Caption = ({ caption }: Artwork) => {
+  return (
+    <p className="py-2 px-2 font-light mx-auto bg-slate-800/30 text-xs md:text-s">
+      {caption}
+    </p>
+  );
+};
