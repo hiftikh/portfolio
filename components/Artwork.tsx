d0fbacc192d21ad3a6ef05a346200765da82b886
Haris Iftikhar <hiftikh@uwo.ca>
2024-10-11 06:24:59 -0400

    added className

--- components/Artwork.tsx
+++ components/Artwork.tsx
@@ -1,3 +1,4 @@
+import { joinClassNames } from "@/lib/utils";
 import Image from "next/image";
 
 interface Artwork {
@@ -6,20 +7,26 @@ interface Artwork {
   alt: string;
   width?: number;
   height?: number;
+  bgColor?: string;
 }
 
 export default function Artwork(params: Artwork) {
   return (
     <>
       <div className="my-10 mx-auto md:w-8/12 text-center">
-        <Image
-          src={params.imageURL}
-          alt={params.alt || "Alt Tag"}
-          width={params.width || 800}
-          height={params.height || 800}
-          className="mx-auto"
-          style={{ height: "auto", width: "auto" }}
-        />
+        <div className={params.bgColor || "bg-transparent"}>
+          <Image
+            src={params.imageURL}
+            alt={params.alt || "Alt Tag"}
+            width={params.width || 800}
+            height={params.height || 800}
+            className={joinClassNames(
+              "mx-auto",
+              params.bgColor || "bg-transparent"
+            )}
+            style={{ height: "auto", width: "auto" }}
+          />
+        </div>
         {params.caption && <Caption {...params} />}
       </div>
     </>
