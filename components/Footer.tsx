a7bbd32b26ad48a960cdc4069d4234c49157ae8f
Haris Iftikhar <hiftikh@uwo.ca>
2024-11-24 03:37:28 -0500

    fixed size to match with <main>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   const footerText = `© ${currentYear} Haris Iftikhar. All Rights Reserved.`;
   return (
     <>
-      <footer className="mt-auto">
+      <footer className="mt-auto w-10/12 lg:max-w-6xl mx-auto">
         <div className="container py-5 mx-auto text-center">
           <hr className="mb-5 border-white/20" />
           <p className="text-secondary text-sm ">{footerText}</p>
