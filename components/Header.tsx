9a16a5f256a220da6321c3a841dc1ed13f00f589
Haris Iftikhar <hiftikh@uwo.ca>
2024-11-23 23:18:59 -0500

    Changed joinClassNames to cn

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,30 @@
 import { Lora } from "next/font/google";
 const font = Lora({ subsets: ["latin"], display: "swap" });
-
-import { joinClassNames } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
 export default function Header(params: any) {
   const textAlign = params.align ? `text-${params.align}` : "";
   const underlineAlign = params.align == "center" ? `mx-auto` : "";
 
-  const className = joinClassNames(
+  const className = cn(
     "text-4xl font-bold py-4",
     `${params.className}`,
     `${textAlign}`
   );
 
   return (
-    <>
-      <div className="mb-10">
-        <header className={font.className}>
-          <h1 className={className}>{params.children || "Header Text"}</h1>
-          {params.underline && (
-            <div
-              className={joinClassNames(
-                "w-2/12 underline h-1 bg-accent border-accent",
-                underlineAlign
-              )}
-            ></div>
-          )}
-        </header>
-      </div>
-    </>
+    <div className="mb-10">
+      <header className={font.className}>
+        <h1 className={className}>{params.children || "Header Text"}</h1>
+        {params.underline && (
+          <div
+            className={cn(
+              "w-2/12 underline h-1 bg-accent border-accent",
+              underlineAlign
+            )}
+          ></div>
+        )}
+      </header>
+    </div>
   );
 }
